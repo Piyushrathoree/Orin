17fd492d86a86dbff8a0b0e5ddf1ef2d1bae33ee
Piyushrathoree <meonpath008@gmail.com>
2026-04-21 03:42:59 +0530

    init app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Figtree ,Spline_Sans_Mono} from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const figtree = Figtree({ subsets: ["latin"], variable: "--font-sans" });
+
+const mono = Spline_Sans_Mono({
+  variable: "--font-mono",
   subsets: ["latin"],
-});
+  weight: ["400", "500", "600", "700"],
+}); 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +27,25 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full",
+        "antialiased",
+        mono.variable,
+        "font-sans", 
+        figtree.variable,
+      )}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TooltipProvider>{children}</TooltipProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
